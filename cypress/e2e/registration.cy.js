33b5147913331327d8057bef6fc639f4e5b7ff20
Rahul Sharma <rahulsharma.rsh@gmail.com>
2022-12-07 18:24:37 +0000

    UKMCAB-347 - OPSS User can Request to Create Account

--- cypress/e2e/registration.cy.js
+++ cypress/e2e/registration.cy.js
@@ -3,6 +3,7 @@ import * as Registration from '../support/helpers/registration-helpers'
 import { shouldBeLoggedIn, shouldBeLoggedOut } from '../support/helpers/common-helpers'
 import OGDUser from '../support/domain/ogd-user'
 import UKASUser from '../support/domain/ukas-user'
+import OpssAdminUser from '../support/domain/opss-admin-user'
 
 describe('Registration', () => {
   
@@ -26,14 +27,14 @@ describe('Registration', () => {
 
     it('is successful if all details are supplied correctly', () => {
       Registration.registerAsOgdUser(new OGDUser())
-      cy.contains('Register email confirmation')
+      Registration.hasConfirmYourEmailMessage()
     })
 
     it('when successful sends verification email to user to verify email ', () => {
       const user = new OGDUser()
       Registration.registerAsOgdUser(user)
       Registration.verifyEmail(user.email)
-      cy.contains('Registration confirmation Your registration request will be reviewed and you will receive notification once approved.')
+      Registration.hasPendingApprovalMessage()
     })
 
     it('displays expected list of legislative areas', () => {
@@ -46,50 +47,50 @@ describe('Registration', () => {
     })
 
     it('is unsuccessful if email used is already taken', () => {
-      const user = new OGDUser({email: `Unknown${Date.now()}@ukmcab.gov.uk`})
+      const user = new OGDUser({email: `OgdUser${Date.now()}@ukmcab.gov.uk`})
       Registration.registerAsOgdUser(user)
       Registration.verifyEmail(user.email)
       Registration.registerAsOgdUser(user)
-      hasFormError('This email address has already been registered on the system.')
+      hasFormError(Registration.errors.emailAlreadyRegistered)
     })
 
 
     it('displays error messages if mandatory details are not supplied', () => {
       Registration.submitForm()
-      hasFieldError('Email', 'The Email field is required.')
-      hasFieldError('Password', 'The Password field is required.')
-      hasFieldError('Confirm password', 'The Confirm password field is required.')
-      hasFieldError('Legislative area', 'Please select at least one legislative area from the list.')
-      hasFieldError('Reason for request', 'Please enter a reason for the request.')
+      hasFieldError('Email', Registration.errors.emailRequired)
+      hasFieldError('Password', Registration.errors.passwordRequired)
+      hasFieldError('Confirm password', Registration.errors.confirmPasswordRequired)
+      hasFieldError('Legislative area', Registration.errors.selectLegislativeArea)
+      hasFieldError('Reason for request', Registration.errors.enterRequestReason)
     })
 
     it('displays error if password does not comply with GDS standard', () => {
       const user = new OGDUser()
       user.password = 'Pass!'
       Registration.registerAsOgdUser(user)
-      hasFormError("The Password must be at least 8 and at max 100 characters long.")
+      hasFormError(Registration.errors.passwordLength)
 
       user.password = 'Pass!Pass@'
       Registration.registerAsOgdUser(user)
-      hasFormError("Passwords must have at least one digit ('0'-'9').")
+      hasFormError(Registration.errors.passwordAtleastOneDigit)
 
       user.password = 'password3@'
       Registration.registerAsOgdUser(user)
-      hasFormError("Passwords must have at least one uppercase ('A'-'Z').")
+      hasFormError(Registration.errors.passwordAtleastOneUppercase)
 
       user.password = 'Password3'
       Registration.registerAsOgdUser(user)
-      hasFormError("Passwords must have at least one non alphanumeric character.")
+      hasFormError(Registration.errors.passwordAtleastOneNonAlphanum)
 
       Registration.enterPasswords('Som3P255W0rd!', 'Som3OTHERP255W0rd!')
       Registration.submitForm()
-      hasFormError("The password and confirmation password do not match.")
+      hasFormError(Registration.errors.passwordsDontMatch)
     })
 
     it('displays error if non GOV UK email address is used', () => {
       const user = new OGDUser({email: '12345@ukmcab.gov.sco'})
       Registration.registerAsOgdUser(user)
-      hasFieldError('Email', 'Only GOV UK email addresses can register for an OGD user account')
+      hasFieldError('Email', Registration.errors.onlyGovUkEmailAllowedForOGD)
     })
   })
 
@@ -101,14 +102,15 @@ describe('Registration', () => {
 
     it('is successful if all details are supplied correctly', () => {
       Registration.registerAsUkasUser(new UKASUser())
-      cy.contains('Register email confirmation')
+      Registration.hasConfirmYourEmailMessage()
+
     })
 
     it('when successful triggers email confirmation to the user', () => {
       const user = new UKASUser()
       Registration.registerAsUkasUser(user)
       Registration.verifyEmail(user.email)
-      cy.contains('Registration confirmation You will now be able to login to your account.')
+      Registration.hasConfirmation()
     })
 
     it('when successful and email verified allows user to login', () => {
@@ -124,7 +126,7 @@ describe('Registration', () => {
       Registration.registerAsUkasUser(user)
       cy.login(user.email, user.password)
       shouldBeLoggedOut()
-      hasFormError('Invalid login attempt.')
+      hasFormError(Registration.errors.invalidLoginAttempt)
     })
 
     it('is unsuccessful if email used is already taken', () => {
@@ -132,43 +134,118 @@ describe('Registration', () => {
       Registration.registerAsUkasUser(user)
       Registration.verifyEmail(user.email)
       Registration.registerAsUkasUser(user)
-      hasFormError('This email address has already been registered on the system.')
+      hasFormError(Registration.errors.emailAlreadyRegistered)
     })
 
     it('displays error messages if mandatory details are not supplied', () => {
       Registration.submitForm()
-      hasFieldError('Email', 'The Email field is required.')
-      hasFieldError('Password', 'The Password field is required.')
-      hasFieldError('Confirm password', 'The Confirm password field is required.')
+      hasFieldError('Email', Registration.errors.emailRequired)
+      hasFieldError('Password', Registration.errors.passwordRequired)
+      hasFieldError('Confirm password', Registration.errors.confirmPasswordRequired)
     })
 
     it('displays error if password does not comply with GDS standard', () => {
       const user = new UKASUser()
       user.password = 'Pass!'
       Registration.registerAsUkasUser(user)
-      hasFormError("The Password must be at least 8 and at max 100 characters long.")
+      hasFormError(Registration.errors.passwordLength)
 
       user.password = 'Pass!Pass@'
       Registration.registerAsUkasUser(user)
-      hasFormError("Passwords must have at least one digit ('0'-'9').")
+      hasFormError(Registration.errors.passwordAtleastOneDigit)
 
       user.password = 'password3@'
       Registration.registerAsUkasUser(user)
-      hasFormError("Passwords must have at least one uppercase ('A'-'Z').")
+      hasFormError(Registration.errors.passwordAtleastOneUppercase)
 
       user.password = 'Password3'
       Registration.registerAsUkasUser(user)
-      hasFormError("Passwords must have at least one non alphanumeric character.")
+      hasFormError(Registration.errors.passwordAtleastOneNonAlphanum)
 
       Registration.enterPasswords('Som3P255W0rd!', 'Som3OTHERP255W0rd!')
       Registration.submitForm()
-      hasFormError("The password and confirmation password do not match.")
+      hasFormError(Registration.errors.passwordsDontMatch)
     })
 
     it('displays error if non GOV UK email address is used', () => {
       const user = new UKASUser({email: '12345@ukmcab.gov.sco'})
       Registration.registerAsUkasUser(user)
-      hasFieldError('Email', 'Only ukas.com email addresses can register for an UKAS user account')
+      hasFieldError('Email', Registration.errors.onlyUkasEmailAllowed)
+    })
+  })
+
+  context('as an OPSS Admin user', () => {
+
+    beforeEach(() => {
+      cy.ensureOn(Registration.registerOpssAdminUserPath())
+    })
+
+    it('is successful if all details are supplied correctly', () => {
+      Registration.registerAsOpssAdminUser(new OpssAdminUser())
+      Registration.hasConfirmYourEmailMessage()
+    })
+
+    it('when successful sends verification email to user to verify email ', () => {
+      const user = new OpssAdminUser()
+      Registration.registerAsOpssAdminUser(user)
+      Registration.verifyEmail(user.email)
+      Registration.hasPendingApprovalMessage()
+    })
+
+    it('displays expected list of legislative areas', () => {
+      cy.contains('Legislative areas').next().within(() => {
+        cy.get('label').then($labels => {
+          const legislativeAreas = Array.from($labels, $label => $label.innerText)
+          expect(legislativeAreas).to.deep.equal(['Cableway Installation', 'Construction Products', 'Ecodesign', 'Electromagnetic Compatibility', 'Equipment and protective systems for use in potentially explosive atmospheres', 'Explosives', 'Gas appliances and related', 'Lifts', 'Machinery', 'Marine equipment', 'Measuring instruments', 'Medical devices', 'Noise emissions in the environment by equipment for use outdoors', 'Non-automatic weighing instruments', 'Personal protective equipment', 'Pressure equipment', 'Pyrotechnics', 'Radio equipment', 'Railway interoperability', 'Recreational craft', 'Simple pressure vessels', 'Toys', 'Transportable pressure equipment'])
+        })
+      })
+    })
+
+    it('is unsuccessful if email used is already taken', () => {
+      const user = new OpssAdminUser({email: `OpssAdminUser${Date.now()}@ukmcab.gov.uk`})
+      Registration.registerAsOpssAdminUser(user)
+      Registration.verifyEmail(user.email)
+      Registration.registerAsOpssAdminUser(user)
+      hasFormError(Registration.errors.emailAlreadyRegistered)
+    })
+
+
+    it('displays error messages if mandatory details are not supplied', () => {
+      Registration.submitForm()
+      hasFieldError('Email', Registration.errors.emailRequired)
+      hasFieldError('Password', Registration.errors.passwordRequired)
+      hasFieldError('Confirm password', Registration.errors.confirmPasswordRequired)
+      hasFieldError('Legislative area', Registration.errors.selectLegislativeArea)
+      hasFieldError('Reason for request', Registration.errors.enterRequestReason)
+    })
+
+    it('displays error if password does not comply with GDS standard', () => {
+      const user = new OpssAdminUser()
+      user.password = 'Pass!'
+      Registration.registerAsOpssAdminUser(user)
+      hasFormError(Registration.errors.passwordLength)
+
+      user.password = 'Pass!Pass@'
+      Registration.registerAsOpssAdminUser(user)
+      hasFormError(Registration.errors.passwordAtleastOneDigit)
+
+      user.password = 'password3@'
+      Registration.registerAsOpssAdminUser(user)
+      hasFormError(Registration.errors.passwordAtleastOneUppercase)
+
+      user.password = 'Password3'
+      Registration.registerAsOpssAdminUser(user)
+      hasFormError(Registration.errors.passwordAtleastOneNonAlphanum)
+
+      Registration.enterPasswords('Som3P255W0rd!', 'Som3OTHERP255W0rd!')
+      Registration.submitForm()
+      hasFormError(Registration.errors.passwordsDontMatch)
+    })
+
+    it('displays error if non GOV UK email address is used', () => {
+      const user = new OpssAdminUser({email: '12345@ukmcab.gov.sco'})
+      Registration.registerAsOgdOrOpssAdminUser(user)
+      hasFieldError('Email', Registration.errors.onlyGovUkEmailAllowedForOPSS)
     })
   })
 
--- cypress/support/helpers/registration-helpers.js
+++ cypress/support/helpers/registration-helpers.js
@@ -3,8 +3,25 @@ export const registerOgdUserPath = () => { return '/account/Register/request/OGD
 export const registerUkasUserPath = () => { return '/account/Register/request/UKASUser' }
 export const registerOpssAdminUserPath = () => { return '/account/Register/request/OPSSAdmin' }
 
-export const registerAsOgdUser = (user) => {
-  cy.ensureOn(registerOgdUserPath())
+export const errors = {
+  emailAlreadyRegistered: 'This email address has already been registered on the system.',
+  emailRequired: 'The Email field is required.',
+  passwordRequired: 'The Password field is required.',
+  confirmPasswordRequired: 'The Confirm password field is required.',
+  passwordLength: 'The Password must be at least 8 and at max 100 characters long.',
+  passwordAtleastOneDigit: "Passwords must have at least one digit ('0'-'9').",
+  passwordAtleastOneUppercase: "Passwords must have at least one uppercase ('A'-'Z').",
+  passwordAtleastOneNonAlphanum: "Passwords must have at least one non alphanumeric character.",
+  passwordsDontMatch: "Passwords must have at least one non alphanumeric character.",
+  onlyUkasEmailAllowed: "Only ukas.com email addresses can register for an UKAS user account",
+  onlyGovUkEmailAllowedForOGD: "Only GOV UK email addresses can register for an OGD user account",
+  onlyGovUkEmailAllowedForOPSS: "Only GOV UK email addresses can register for an OPSS admin account",
+  selectLegislativeArea: "Please select at least one legislative area from the list.",
+  enterRequestReason: "Please enter a reason for the request.",
+  invalidLoginAttempt: "Invalid login attempt.",
+}
+
+export const registerAsOgdOrOpssAdminUser = (user) => {
   cy.get('#Email').invoke('val', user.email)
   enterPasswords(user.password)
   user.legislativeAreas.forEach(area => {
@@ -12,6 +29,16 @@ export const registerAsOgdUser = (user) => {
   })
   cy.get('#RequestReason').invoke('val', user.requestReason)
   submitForm()
+}
+
+export const registerAsOgdUser = (user) => {
+  cy.ensureOn(registerOgdUserPath())
+  registerAsOgdOrOpssAdminUser(user)
+}
+
+export const registerAsOpssAdminUser = (user) => {
+  cy.ensureOn(registerOpssAdminUserPath())
+  registerAsOgdOrOpssAdminUser(user)
 }
 
 export const registerAsUkasUser = (user) => {
@@ -31,10 +58,21 @@ export const submitForm = () => {
   cy.contains('button', 'Request account').click()
 }
 
+export const hasConfirmYourEmailMessage = () => {
+  cy.contains('Register email confirmation Please check your email to confirm your account.')
+}
+
+export const hasPendingApprovalMessage = () => {
+  cy.contains('Registration confirmation Your registration request will be reviewed and you will receive notification once approved.')
+}
+
+export const hasConfirmation = () => {
+  cy.contains('Registration confirmation You will now be able to login to your account.')
+}
+
 export const verifyEmail = (email) => {
   cy.task('getLastEmail', email).then(email => {
     const verificationLink = email.body.match(/^https(.*)$/gm)[0]
     cy.ensureOn(verificationLink)
-    cy.contains('Registration confirmation')
   })
 }
