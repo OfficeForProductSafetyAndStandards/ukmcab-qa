f9c3777daeb5e28c47bdc34c13bd2c3db8de8b57
Rahul Sharma <rahulsharma.rsh@gmail.com>
2023-04-21 11:19:55 +0100

    UKMCAB-572 - Contact link updates

--- cypress/e2e/footer.cy.js
+++ cypress/e2e/footer.cy.js
@@ -7,7 +7,7 @@ describe('Footer', function() {
     footer().contains('a', 'Privacy').should('have.attr', 'href', '/privacy-notice')
     footer().contains('a', 'Cookies').should('have.attr', 'href', '/cookies-policy')
     footer().contains('a', 'Accessibility statement').should('have.attr', 'href', '/accessibility-statement')
-    footer().contains('a', 'Contact').should('have.attr', 'href', 'https://www.gov.uk/contact').and('have.attr', 'target', '_blank')
+    footer().contains('a', 'Contact').should('have.attr', 'href', '/help#contact-info')
     footer().contains('a', 'Terms and conditions').should('have.attr', 'href', '/terms-and-conditions')
     footer().contains('a', 'Government Digital Service').should('have.attr', 'href', 'https://www.gov.uk/government/organisations/government-digital-service').and('have.attr', 'target', '_blank')
     footer().contains('All content is available under the Open Government Licence v3.0 (opens in a new window), except where otherwise stated').find('a', 'Open Government Licence v3.0').should('have.attr', 'href', 'https://www.nationalarchives.gov.uk/doc/open-government-licence/version/3/').and('have.attr', 'target', '_blank')
