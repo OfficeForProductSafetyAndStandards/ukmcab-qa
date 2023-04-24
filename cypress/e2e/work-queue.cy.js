d9ff8b943a8714f57cbf110da621ca1073e3fc6d
Rahul Sharma <rahulsharma.rsh@gmail.com>
2023-04-24 10:59:05 +0100

    UKMCAB-789 - Internal User "Work Queue" Needs Pagination and Resorting

--- cypress/e2e/work-queue.cy.js
+++ cypress/e2e/work-queue.cy.js
@@ -3,19 +3,23 @@ import * as CabHelpers from '../support/helpers/cab-helpers'
 
 describe('Work Queue', function() {
 
-  const sortCabsByNameAsc = (cabs) => {
+  const sortedByLastUpdatedDesc = (cabs) => {
+    return cabs.sort(c => c.lastUpdatedDate).reverse()
+  }
+
+  const sortedByNameAsc = (cabs) => {
     return cabs.sort((a,b) => a.name.localeCompare(b.name) || a.createdDate - b.createdDate)
   }
 
-  const sortCabsByNameDesc = (cabs) => {
+  const sortedByNameDesc = (cabs) => {
     return cabs.sort((a,b) => b.name.localeCompare(a.name) || a.createdDate - b.createdDate)
   }
 
-  const sortCabsByNumberAsc = (cabs) => {
+  const sortedByNumberAsc = (cabs) => {
     return cabs.sort((a,b) => a.cabNumber.localeCompare(b.cabNumber) || a.createdDate - b.createdDate)
   }
 
-  const sortCabsByNumberDesc = (cabs) => {
+  const sortedByNumberDesc = (cabs) => {
     return cabs.sort((a,b) => b.cabNumber.localeCompare(a.cabNumber) || a.createdDate - b.createdDate)
   }
 
@@ -34,8 +38,8 @@ describe('Work Queue', function() {
     cy.get('a.govuk-button').should('contain', 'Create a CAB').and('has.attr', 'href', addCabPath())
   })
 
-  it('displays All(Draft or Archived) CABs sorted by Name by default', function() {
-    sortCabsByNameAsc(this.cabs).forEach((cab, index) => {
+  it('displays All(Draft or Archived) CABs sorted by Last Updated Date by default', function() {
+    sortedByLastUpdatedDesc(this.cabs).slice(0,10).forEach((cab, index) => {
       cy.get('tbody > tr.govuk-table__row').eq(index).within(() => {
         cy.get('td').eq(0).contains(cab.name).and('has.attr', 'href', cabSummaryPage(cab.cabId))
         cy.get('td').eq(1).contains(cab.cabNumber)
@@ -44,9 +48,14 @@ describe('Work Queue', function() {
     })
   })
 
+  it('displays pagination with 10 cabs per page', function() {
+    cy.get('tbody > tr.govuk-table__row').should('have.length', 10)
+    cy.get('.pagination-detail-container').contains(`Showing 1 - ${this.cabs.slice(0,10).length} of ${this.cabs.length} items`)
+  })
+
   it('displays correct CAB details when filtered by Draft or Archived', function() {
     cy.get('#Filter').select('Draft')
-    sortCabsByNameAsc(this.draftCabs).forEach((cab, index) => {
+    sortedByLastUpdatedDesc(this.draftCabs).slice(0,10).forEach((cab, index) => {
       cy.get('tbody > tr.govuk-table__row').eq(index).within(() => {
         cy.get('td').eq(0).contains(cab.name).and('has.attr', 'href', cabSummaryPage(cab.cabId))
         cy.get('td').eq(1).contains(cab.cabNumber)
@@ -65,8 +74,16 @@ describe('Work Queue', function() {
   })
 
   it('displays correct CAB order upon sorting by any of the sortable columns', function() {
+    cy.get('thead th a').eq(0).click() // CAB name Asc sort
+    sortedByNameAsc(this.cabs).slice(0,10).forEach((cab, index) => {
+      cy.get('tbody > tr.govuk-table__row').eq(index).within(() => {
+        cy.get('td').eq(0).contains(cab.name).and('has.attr', 'href', cabSummaryPage(cab.cabId))
+        cy.get('td').eq(1).contains(cab.cabNumber)
+        cy.get('td').eq(2).contains(cab.status)
+      })
+    })
     cy.get('thead th a').eq(0).click() // CAB name desc sort
-    sortCabsByNameDesc(this.cabs).forEach((cab, index) => {
+    sortedByNameDesc(this.cabs).slice(0,10).forEach((cab, index) => {
       cy.get('tbody > tr.govuk-table__row').eq(index).within(() => {
         cy.get('td').eq(0).contains(cab.name).and('has.attr', 'href', cabSummaryPage(cab.cabId))
         cy.get('td').eq(1).contains(cab.cabNumber)
@@ -74,7 +91,7 @@ describe('Work Queue', function() {
       })
     })
     cy.get('thead th a').eq(1).click() // CAB Number asc sort
-    sortCabsByNumberAsc(this.cabs).forEach((cab, index) => {
+    sortedByNumberAsc(this.cabs).slice(0,10).forEach((cab, index) => {
       cy.get('tbody > tr.govuk-table__row').eq(index).within(() => {
         cy.get('td').eq(0).contains(cab.name).and('has.attr', 'href', cabSummaryPage(cab.cabId))
         cy.get('td').eq(1).contains(cab.cabNumber)
@@ -82,7 +99,7 @@ describe('Work Queue', function() {
       })
     })
     cy.get('thead th a').eq(1).click() // CAB Number desc sort
-    sortCabsByNumberDesc(this.cabs).forEach((cab, index) => {
+    sortedByNumberDesc(this.cabs).slice(0,10).forEach((cab, index) => {
       cy.get('tbody > tr.govuk-table__row').eq(index).within(() => {
         cy.get('td').eq(0).contains(cab.name).and('has.attr', 'href', cabSummaryPage(cab.cabId))
         cy.get('td').eq(1).contains(cab.cabNumber)
