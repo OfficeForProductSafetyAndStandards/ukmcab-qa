f17200ef698d0dacb0bd31cfb92afc72a08b31ab
Rahul Sharma <rahulsharma.rsh@gmail.com>
2023-07-04 10:35:10 +0100

    UKMCAB-932 > The search link from the menu points to /search and not the main URL

--- cypress/e2e/header.cy.js
+++ cypress/e2e/header.cy.js
@@ -1,5 +1,4 @@
 import { header } from '../support/helpers/common-helpers'
-import { searchPath } from '../support/helpers/search-helpers'
 import { loginPath } from '../support/helpers/login-helpers'
 import { changePasswordPath } from '../support/helpers/password-helpers'
 import { cabManagementPath } from '../support/helpers/cab-helpers'
@@ -16,7 +15,7 @@ describe('Header', function() {
     it('displays all expected links', function() {
       header().contains('a', 'GOV.UK').should('have.attr', 'href', '/')
       header().contains('a', 'UK Market Conformity Assessment Bodies').should('have.attr', 'href', '/')
-      header().contains('a', 'Search').should('have.attr', 'href', searchPath())
+      header().contains('a', 'Search').should('have.attr', 'href', '/')
       header().contains('a', 'Help').should('have.attr', 'href', '/help')
       header().contains('a', 'Updates').should('have.attr', 'href', '/updates')
       header().contains('a', 'About').should('have.attr', 'href', '/about')
@@ -34,7 +33,7 @@ describe('Header', function() {
     it('displays all expected links', function() {
       header().contains('a', 'GOV.UK').should('have.attr', 'href', '/')
       header().contains('a', 'UK Market Conformity Assessment Bodies').should('have.attr', 'href', '/')
-      header().contains('a', 'Search').should('have.attr', 'href', searchPath())
+      header().contains('a', 'Search').should('have.attr', 'href', '/')
       header().contains('a', 'Help').should('have.attr', 'href', '/help')
       header().contains('a', 'About').should('have.attr', 'href', '/about')
       header().contains('a', 'Updates').should('have.attr', 'href', '/updates')
