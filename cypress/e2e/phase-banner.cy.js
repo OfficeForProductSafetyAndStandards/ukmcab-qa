6ccc4f495482adec8090ae9e2bbc5a17a2d7273d
Rahul Sharma <rahulsharma.rsh@gmail.com>
2023-07-04 10:30:45 +0100

    added return url

--- cypress/e2e/phase-banner.cy.js
+++ cypress/e2e/phase-banner.cy.js
@@ -5,6 +5,6 @@ describe('Phase Banner', () => {
     cy.ensureOn('/')
     cy.get('.govuk-phase-banner__content')
     .contains('beta How could we improve this service? Your feedback will help.')
-    .find('a', 'feedback').should('have.attr', 'href', contactUsUrl())
+    .find('a', 'feedback').should('have.attr', 'href', contactUsUrl() + '?returnUrl=%252F')
   })
 })
