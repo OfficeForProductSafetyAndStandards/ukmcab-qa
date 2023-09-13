e75ef1a9f65bea0c8f48ea9d728027c6db6d27b7
Rahul Sharma <rahulsharma.rsh@gmail.com>
2023-09-13 18:10:20 +0100

    removed obsolete code and refs

--- cypress/support/helpers/login-helpers.js
+++ cypress/support/helpers/login-helpers.js
@@ -6,18 +6,3 @@ export const login = (username, password) => {
   cy.get('#Password').invoke('val', password)
   cy.contains('button', 'Sign in').click()
 }
-
-export const loginAsOpssUser = () => { 
-  cy.ensureOn(loginPath())
-  login(Cypress.env('OPSS_USER'), Cypress.env('OPSS_PASS'))
-}
-
-export const loginAsOgdUser = () => { 
-  cy.ensureOn(loginPath())
-  login(Cypress.env('OGD_USER'), Cypress.env('OGD_PASS'))
-}
-
-export const loginAsUkasUser = () => { 
-  cy.ensureOn(loginPath())
-  login(Cypress.env('UKAS_USER'), Cypress.env('UKAS_PASS'))
-}
