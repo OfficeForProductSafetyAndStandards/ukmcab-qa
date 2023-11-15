659cdb686908cb11f07ddb895136d5b73155e0eb
leo ndi <ndileonel@gmail.com>
2023-11-15 13:09:25 +0000

    re-enable tests

--- cypress/e2e/cab-page.cy.js
+++ cypress/e2e/cab-page.cy.js
@@ -19,18 +19,18 @@ describe('CAB profile page', function () {
       })
     })
 
-    // it('displays CAB name heading', function() {
-    //   cy.get('.govuk-heading-l').contains(this.cab.name)
-    // })
+    it('displays CAB name heading', function() {
+      cy.get('.govuk-heading-l').contains(this.cab.name)
+    })
 
-    // it('has back to search results link', function() {
-    //   cy.get('a').contains('Back to search results').and('has.attr', 'href', searchPath())
-    // })
+    it('has back to search results link', function() {
+      cy.get('a').contains('Back to search results').and('has.attr', 'href', searchPath())
+    })
 
-    // it('displays published and updated date', function() {
-    //   cy.contains(`Published: ${date(this.cab.publishedDate).DMMMYYYY}`)
-    //   cy.contains(`Last updated: ${date(this.cab.lastUpdatedDate).DMMMYYYY}`)
-    // })
+    it('displays published and updated date', function() {
+      cy.contains(`Published: ${date(this.cab.publishedDate).DMMMYYYY}`)
+      cy.contains(`Last updated: ${date(this.cab.lastUpdatedDate).DMMMYYYY}`)
+    })
 
     it('displays all expected CAB details', function() {
       cy.contains('.cab-detail-section', 'CAB details').within(() => {
@@ -62,9 +62,9 @@ describe('CAB profile page', function () {
       })
     })
 
-    // it('does not display Supporting Documents to logged out users', function() {
-    //   supportingDocumentsTab().should('not.exist')
-    // })
+    it('does not display Supporting Documents to logged out users', function() {
+      supportingDocumentsTab().should('not.exist')
+    })
   })
 
 })
@@ -75,15 +75,15 @@ describe('CAB profile page', function () {
     return cy.contains('#tab_supporting-documents', 'Supporting documents')
   }
 
-  // context('when logged in', function () {
+  context('when logged in', function () {
 
-  //   beforeEach(function () {
-  //     CabHelpers.getTestCabWithDocuments().then(cab => {
-  //       cy.wrap(cab).as('cab')
-  //       cy.loginAsOpssUser()
-  //       cy.ensureOn(CabHelpers.cabProfilePage(cab))
-  //     })
-  //   })
+    beforeEach(function () {
+      CabHelpers.getTestCabWithDocuments().then(cab => {
+        cy.wrap(cab).as('cab')
+        cy.loginAsOpssUser()
+        cy.ensureOn(CabHelpers.cabProfilePage(cab))
+      })
+    })
 
     it('displays all expected CAB details', function() {
       cy.contains('.cab-detail-section', 'CAB details').within(() => {
@@ -115,166 +115,166 @@ describe('CAB profile page', function () {
       })
     })
 
-  //   // test commented out due to download flaky but works manually
-  //   it.skip('displays viewable and downloadable list of uploaded schedules', function() {
-  //     CabHelpers.viewSchedules()
-  //     cy.contains('.cab-detail-section', 'Product schedules').within(() => {
+    // test commented out due to download flaky but works manually
+    it.skip('displays viewable and downloadable list of uploaded schedules', function() {
+      CabHelpers.viewSchedules()
+      cy.contains('.cab-detail-section', 'Product schedules').within(() => {
 
-  //       // TODO: GROUP AND SORT LEGISLATIVE AREAS WHEN MULTIPE UPLOAD ISSUE IS FIXED
+        // TODO: GROUP AND SORT LEGISLATIVE AREAS WHEN MULTIPE UPLOAD ISSUE IS FIXED
 
-  //       this.cab.schedules.forEach((schedule,index) => {
-  //         // Known cypress issue with dowbload links timeout  - https://github.com/cypress-io/cypress/issues/14857
-  //         cy.window().then((win) => { setTimeout(() => { win.location.reload() },5000) }) 
-  //         cy.get('.govuk-summary-list__row').eq(index).within(() => {
-  //           cy.get('dt').contains(schedule.label)
-  //           cy.get('dd').eq(0).contains('a', 'View').should('have.attr', 'target', '_blank').invoke('attr', 'href').should('match', /^\/search\/cab-schedule-view/)
-  //           cy.get('dd').eq(1).contains('a', 'Download').click()
-  //           cy.readFile(`cypress/downloads/${schedule.fileName}`)
-  //         })
-  //       })
-  //     })
-  //   })
+        this.cab.schedules.forEach((schedule,index) => {
+          // Known cypress issue with dowbload links timeout  - https://github.com/cypress-io/cypress/issues/14857
+          cy.window().then((win) => { setTimeout(() => { win.location.reload() },5000) }) 
+          cy.get('.govuk-summary-list__row').eq(index).within(() => {
+            cy.get('dt').contains(schedule.label)
+            cy.get('dd').eq(0).contains('a', 'View').should('have.attr', 'target', '_blank').invoke('attr', 'href').should('match', /^\/search\/cab-schedule-view/)
+            cy.get('dd').eq(1).contains('a', 'Download').click()
+            cy.readFile(`cypress/downloads/${schedule.fileName}`)
+          })
+        })
+      })
+    })
 
-  //   // Some mapping has been added that maps legacy(imported) cab schedules to legislative areas.
-  //   // This is done at DB level. This test has picked out an entry from that mapping spreadhseet
-  //   // and validates that all those entries are displayed on the page for this cab.
-  //   // skipping this test as now with Archiving in place this test CAB is often archived on DEV env so tests are flaky
-  //   it.skip('displays correct mapping of legislative areas for legacy cabs', function() {
-  //     const expectedData = [
-  //       {
-  //         "Lifts": 'Lifts regulations 2016'
-  //       },
-  //       {
-  //         "Transportable pressure equipment": 'Transportable Pressure Equipment Regulations 2009'
-  //       },
-  //       {
-  //         "Gas appliances and related": 'Appliances Burning Gas Regulations EU 2016/426 (as retained in UK law and amended)'
-  //       },
-  //       {
-  //         "Measuring instruments": 'Measuring Instruments Regulations 2016'
-  //       },
-  //       {
-  //         "Construction products": 'The Construction Products Regulation 2011 (EU) No 305/2011 as it has effect in the UK as retained EU law and by virtue of the Northern Ireland Protocol'
-  //       },
-  //       {
-  //         "Marine equipment": 'Marine Equipment Regulations 2016'
-  //       },
-  //       {
-  //         "Personal protective equipment": 'Personal Protective Equipment Regulations (Regulation (EU) 2016/425 as brought into UK law and amended) and the Personal Protective Equipment (Enforcement) Regulations 2018'
-  //       },
-  //       {
-  //         "Pressure equipment": 'Pressure Equipment (Safety) Regulations 2016'
-  //       },
-  //       {
-  //         "Radio equipment": 'Radio Equipment Regulations 2017'
-  //       },
-  //       {
-  //         "Ecodesign": 'Ecodesign (Boiler Efficiency Regulations)'
-  //       },
-  //       {
-  //         "Medical devices": 'UK-Australia MRA'
-  //       }
-  //     ]
-  //     cy.ensureOn('/search/cab-profile/bsi-assurance-uk-ltd')
-  //     CabHelpers.viewSchedules()
-  //     expectedData.forEach(data => {
-  //       Object.entries(data).forEach(entry => {
-  //         cy.contains('h3', entry[0]).next('dl').contains(entry[1])
-  //       })
-  //     })
-  //   })
+    // Some mapping has been added that maps legacy(imported) cab schedules to legislative areas.
+    // This is done at DB level. This test has picked out an entry from that mapping spreadhseet
+    // and validates that all those entries are displayed on the page for this cab.
+    // skipping this test as now with Archiving in place this test CAB is often archived on DEV env so tests are flaky
+    it.skip('displays correct mapping of legislative areas for legacy cabs', function() {
+      const expectedData = [
+        {
+          "Lifts": 'Lifts regulations 2016'
+        },
+        {
+          "Transportable pressure equipment": 'Transportable Pressure Equipment Regulations 2009'
+        },
+        {
+          "Gas appliances and related": 'Appliances Burning Gas Regulations EU 2016/426 (as retained in UK law and amended)'
+        },
+        {
+          "Measuring instruments": 'Measuring Instruments Regulations 2016'
+        },
+        {
+          "Construction products": 'The Construction Products Regulation 2011 (EU) No 305/2011 as it has effect in the UK as retained EU law and by virtue of the Northern Ireland Protocol'
+        },
+        {
+          "Marine equipment": 'Marine Equipment Regulations 2016'
+        },
+        {
+          "Personal protective equipment": 'Personal Protective Equipment Regulations (Regulation (EU) 2016/425 as brought into UK law and amended) and the Personal Protective Equipment (Enforcement) Regulations 2018'
+        },
+        {
+          "Pressure equipment": 'Pressure Equipment (Safety) Regulations 2016'
+        },
+        {
+          "Radio equipment": 'Radio Equipment Regulations 2017'
+        },
+        {
+          "Ecodesign": 'Ecodesign (Boiler Efficiency Regulations)'
+        },
+        {
+          "Medical devices": 'UK-Australia MRA'
+        }
+      ]
+      cy.ensureOn('/search/cab-profile/bsi-assurance-uk-ltd')
+      CabHelpers.viewSchedules()
+      expectedData.forEach(data => {
+        Object.entries(data).forEach(entry => {
+          cy.contains('h3', entry[0]).next('dl').contains(entry[1])
+        })
+      })
+    })
 
-  //   it('displays viewable and downloadable list of supporting documents', function () {
-  //     supportingDocumentsTab().click()
-  //     cy.contains('.cab-detail-section', 'Supporting documents').within(() => {
-  //       this.cab.documents.sort((a, b) => a.fileName.localeCompare(b.fileName)).forEach((document, index) => {
-  //         // Known cypress issue with dowbload links timeout  - https://github.com/cypress-io/cypress/issues/14857
-  //         cy.window().then((win) => { setTimeout(() => { win.location.reload() }, 5000) })
-  //         cy.get('.govuk-summary-list__row').eq(index).within(() => {
-  //           cy.get('dt').contains(document.fileName)
-  //           if (document.fileName.endsWith('.pdf')) {
-  //             cy.get('dd').eq(0).contains('a', 'View').should('have.attr', 'target', '_blank').invoke('attr', 'href').should('match', /^\/search\/cab-schedule-view/)
-  //           } else {
-  //             cy.get('dd').eq(0).contains('a', 'View').should('not.exist')
-  //           }
-  //           cy.get('dd').eq(1).contains('a', 'Download').click()
-  //           cy.readFile(`cypress/downloads/${document.fileName}`)
-  //         })
-  //       })
-  //     })
-  //   })
+    it('displays viewable and downloadable list of supporting documents', function () {
+      supportingDocumentsTab().click()
+      cy.contains('.cab-detail-section', 'Supporting documents').within(() => {
+        this.cab.documents.sort((a, b) => a.fileName.localeCompare(b.fileName)).forEach((document, index) => {
+          // Known cypress issue with dowbload links timeout  - https://github.com/cypress-io/cypress/issues/14857
+          cy.window().then((win) => { setTimeout(() => { win.location.reload() }, 5000) })
+          cy.get('.govuk-summary-list__row').eq(index).within(() => {
+            cy.get('dt').contains(document.fileName)
+            if (document.fileName.endsWith('.pdf')) {
+              cy.get('dd').eq(0).contains('a', 'View').should('have.attr', 'target', '_blank').invoke('attr', 'href').should('match', /^\/search\/cab-schedule-view/)
+            } else {
+              cy.get('dd').eq(0).contains('a', 'View').should('not.exist')
+            }
+            cy.get('dd').eq(1).contains('a', 'Download').click()
+            cy.readFile(`cypress/downloads/${document.fileName}`)
+          })
+        })
+      })
+    })
 
-  //   it('displays paginated Cab history ordered by latest first', function () {
-  //     CabHelpers.viewHistory()
-  //     // Assuming you have previously defined and wrapped 'cabs' using cy.wrap()
-  //     // cy.contains(`Showing 1 - ${this.cab.auditLog.slice(0, 10).length} of ${this.cab.auditLog.length}`)
+    it('displays paginated Cab history ordered by latest first', function () {
+      CabHelpers.viewHistory()
+      // Assuming you have previously defined and wrapped 'cabs' using cy.wrap()
+      // cy.contains(`Showing 1 - ${this.cab.auditLog.slice(0, 10).length} of ${this.cab.auditLog.length}`)
       
-  //     cy.log(`APproved user is: ${this.cab.auditLog.UserName}, ${this.cab.auditLog.UserRole}`)
+      cy.log(`APproved user is: ${this.cab.auditLog.UserName}, ${this.cab.auditLog.UserRole}`)
 
-  //     cy.wrap(Cypress._.orderBy(this.cab.auditLog, 'DateTime', 'desc').slice(0, 10)).each((logvalue, index) => {
-  //       cy.get('tbody tr').eq(index).within(() => {
-  //         cy.get('td').eq(0).contains(Cypress.dayjs(logvalue.DateTime).utc().format('DD/MM/YYYYHH:mm'))
-  //         cy.get('td').eq(1).contains(logvalue.UserName)
-  //         cy.get('td').eq(2).contains(logvalue.UserRole.toUpperCase())
-  //         cy.get('td').eq(3).contains(Cypress._.capitalize(Cypress._.startCase(logvalue.Action)))
-  //         if (logvalue.Comment) {
-  //           cy.get('td').eq(4).contains('View more ') // + log.Comment - removed comment check as its seems to be wrapped in HTML. // TODO check with devs
-  //         }
-  //       })
-  //     })
-  //   })
-  // })
+      cy.wrap(Cypress._.orderBy(this.cab.auditLog, 'DateTime', 'desc').slice(0, 10)).each((logvalue, index) => {
+        cy.get('tbody tr').eq(index).within(() => {
+          cy.get('td').eq(0).contains(Cypress.dayjs(logvalue.DateTime).utc().format('DD/MM/YYYYHH:mm'))
+          cy.get('td').eq(1).contains(logvalue.UserName)
+          cy.get('td').eq(2).contains(logvalue.UserRole.toUpperCase())
+          cy.get('td').eq(3).contains(Cypress._.capitalize(Cypress._.startCase(logvalue.Action)))
+          if (logvalue.Comment) {
+            cy.get('td').eq(4).contains('View more ') // + log.Comment - removed comment check as its seems to be wrapped in HTML. // TODO check with devs
+          }
+        })
+      })
+    })
+  })
 
-  // context('for CABs with Cab number visibility set to public(default)', function() {
+  context('for CABs with Cab number visibility set to public(default)', function() {
 
-  //   it('displays Cab(Body) number for public and internal users', function() {
-  //     const cab = Cab.buildWithoutDocuments()
-  //     cy.loginAsOpssUser()
-  //     CabHelpers.createCabWithoutDocuments(cab)
-  //     cy.ensureOn(CabHelpers.cabProfilePage(cab))
-  //     cy.hasKeyValueDetail('CAB number', cab.cabNumber)
-  //     cy.logout()
-  //     cy.ensureOn(CabHelpers.cabProfilePage(cab))
-  //     cy.hasKeyValueDetail('Body number', cab.cabNumber)
-  //   })
-  // })
+    it('displays Cab(Body) number for public and internal users', function() {
+      const cab = Cab.buildWithoutDocuments()
+      cy.loginAsOpssUser()
+      CabHelpers.createCabWithoutDocuments(cab)
+      cy.ensureOn(CabHelpers.cabProfilePage(cab))
+      cy.hasKeyValueDetail('CAB number', cab.cabNumber)
+      cy.logout()
+      cy.ensureOn(CabHelpers.cabProfilePage(cab))
+      cy.hasKeyValueDetail('Body number', cab.cabNumber)
+    })
+  })
 
-  // context('for CABs with Cab number visibility set to internal(Display for all internal)', function() {
+  context('for CABs with Cab number visibility set to internal(Display for all internal)', function() {
 
-  //   it('displays Cab(Body) number for UKAS/OPSS users but not for Public users', function() {
-  //     const cab = Cab.buildWithoutDocuments()
-  //     cab.cabNumberVisibility = CabNumberVisibility.Internal
-  //     cy.loginAsOpssUser()
-  //     CabHelpers.createCabWithoutDocuments(cab)
-  //     cy.ensureOn(CabHelpers.cabProfilePage(cab))
-  //     cy.hasKeyValueDetail('CAB number', cab.cabNumber)
-  //     cy.logout()
-  //     cy.loginAsUkasUser()
-  //     cy.ensureOn(CabHelpers.cabProfilePage(cab))
-  //     cy.hasKeyValueDetail('CAB number', cab.cabNumber)
-  //     cy.logout()
-  //     cy.ensureOn(CabHelpers.cabProfilePage(cab))
-  //     cy.contains('Body number').should('not.exist')
-  //   })
-  // })
+    it('displays Cab(Body) number for UKAS/OPSS users but not for Public users', function() {
+      const cab = Cab.buildWithoutDocuments()
+      cab.cabNumberVisibility = CabNumberVisibility.Internal
+      cy.loginAsOpssUser()
+      CabHelpers.createCabWithoutDocuments(cab)
+      cy.ensureOn(CabHelpers.cabProfilePage(cab))
+      cy.hasKeyValueDetail('CAB number', cab.cabNumber)
+      cy.logout()
+      cy.loginAsUkasUser()
+      cy.ensureOn(CabHelpers.cabProfilePage(cab))
+      cy.hasKeyValueDetail('CAB number', cab.cabNumber)
+      cy.logout()
+      cy.ensureOn(CabHelpers.cabProfilePage(cab))
+      cy.contains('Body number').should('not.exist')
+    })
+  })
 
-  // context('for CABs with Cab number visibility set to private(Display for only internal gov users)', function() {
+  context('for CABs with Cab number visibility set to private(Display for only internal gov users)', function() {
 
-  //   it('displays Cab(Body) number for OPSS users but not for Public/UKAS users', function() {
-  //     const cab = Cab.buildWithoutDocuments()
-  //     cab.cabNumberVisibility = CabNumberVisibility.Private
-  //     cy.loginAsOpssUser()
-  //     CabHelpers.createCabWithoutDocuments(cab)
-  //     cy.ensureOn(CabHelpers.cabProfilePage(cab))
-  //     cy.hasKeyValueDetail('CAB number', cab.cabNumber)
-  //     cy.logout()
-  //     cy.loginAsUkasUser()
-  //     cy.ensureOn(CabHelpers.cabProfilePage(cab))
-  //     cy.contains('CAB number').should('not.exist')
-  //     cy.logout()
-  //     cy.ensureOn(CabHelpers.cabProfilePage(cab))
-  //     cy.contains('Body number').should('not.exist')
-  //   })
-  // })
+    it('displays Cab(Body) number for OPSS users but not for Public/UKAS users', function() {
+      const cab = Cab.buildWithoutDocuments()
+      cab.cabNumberVisibility = CabNumberVisibility.Private
+      cy.loginAsOpssUser()
+      CabHelpers.createCabWithoutDocuments(cab)
+      cy.ensureOn(CabHelpers.cabProfilePage(cab))
+      cy.hasKeyValueDetail('CAB number', cab.cabNumber)
+      cy.logout()
+      cy.loginAsUkasUser()
+      cy.ensureOn(CabHelpers.cabProfilePage(cab))
+      cy.contains('CAB number').should('not.exist')
+      cy.logout()
+      cy.ensureOn(CabHelpers.cabProfilePage(cab))
+      cy.contains('Body number').should('not.exist')
+    })
+  })
 
 })
