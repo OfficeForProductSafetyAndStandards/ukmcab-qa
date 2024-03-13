c84e42b7858a5e2d0eaa42202fecfd907e86caa2
Kabir Yusuf <yusufkabir@hotmail.co.uk>
2024-03-13 12:16:12 +0000

    Implemented E2E test for archive LA and remove LA journies

--- cypress/e2e/archive-la-and-remove-la.cy.js
+++ cypress/e2e/archive-la-and-remove-la.cy.js
@@ -8,6 +8,7 @@ describe("Request to Archive an LA and to Remove an LA", () => {
   context("Archive an LA", function () {
     const uniqueId = Date.now();
     const name = `Test Cab ${uniqueId}`;
+    var cabId = "";
 
     beforeEach(function () {
       cy.wrap(Cab.buildWithoutDocuments()).as("cab");
@@ -19,27 +20,36 @@ describe("Request to Archive an LA and to Remove an LA", () => {
     it("UKAS creates and submits for approval a request to publish a cab", function () {
       cy.loginAsUkasUser();
       CabHelpers.createAndSubmitCabForApproval(name, this.cab);
-      cy.hasStatus("Pending approval to publish CAB");
-      cy.url().as("draftUrl");
+      cy.url()
+        .as("draftUrl")
+        .then((url) => {
+          cabId = CabHelpers.getIdFromLastPartOfUrl(url);
+        });
       cy.logout();
+    });
+
+    it("OPSS approves and publish CAB requested by UKAS", function () {
+      cy.loginAsOpssUser();
+      CabHelpers.opssApprovesCAB(this.draftUrl, uniqueId);
+      cy.ensureOn(CabHelpers.cabProfileUrlPathByCabName(name));
+      cy.hasStatus("Published");
     });
 
     it("OPSS archives an LA", function () {
       cy.loginAsOpssUser();
-      CabHelpers.opssArchiveCAB(name);
+      CabHelpers.opssArchiveLA(cabId);
       cy.logout();
     });
   });
 
-  context("and Delete", function () {
+  context("Remove an LA", function () {
     const uniqueId = Date.now();
-    const name = `Test Cab ${uniqueId}`;
-    const declinedReason = `The request to unarchive CAB ${name} has been declined.`;
+    const name = `Test Kab ${uniqueId}`;
+    var cabId = "";
 
     beforeEach(function () {
       cy.wrap(Cab.buildWithoutDocuments()).as("cab");
       UserManagementHelpers.getTestUsers().then((users) => {
-        cy.wrap(users.OpssAdminUser).as("opssAdminUser");
         cy.wrap(users.UkasUser).as("ukasUser");
       });
     });
@@ -47,13 +57,24 @@ describe("Request to Archive an LA and to Remove an LA", () => {
     it("UKAS creates and submits for approval a request to publish a cab", function () {
       cy.loginAsUkasUser();
       CabHelpers.createAndSubmitCabForApproval(name, this.cab);
-      cy.url().as("draftUrl");
+      cy.url()
+        .as("draftUrl")
+        .then((url) => {
+          cabId = CabHelpers.getIdFromLastPartOfUrl(url);
+        });
       cy.logout();
     });
 
-    it("OPSS archive CAB", function () {
+    it("OPSS approves and publish CAB requested by UKAS", function () {
       cy.loginAsOpssUser();
-      CabHelpers.opssArchiveCAB(name);
+      CabHelpers.opssApprovesCAB(this.draftUrl, uniqueId);
+      cy.ensureOn(CabHelpers.cabProfileUrlPathByCabName(name));
+      cy.hasStatus("Published");
+    });
+
+    it("OPSS removes an LA", function () {
+      cy.loginAsOpssUser();
+      CabHelpers.opssRemoveLA(cabId);
       cy.logout();
     });
   });
